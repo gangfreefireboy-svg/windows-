e18c3ce4a82a73920adeae67dfd0e21c5ce3d4e1
!! @🐸FʀᴏɢɢʏLIVE ⌁ <gangfreefireboy@gmail.com>
2026-09-09 22:39:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,49 @@
-FROM debian:bookworm
+#!/bin/bash
+set -e
 
-ENV DEBIAN_FRONTEND=noninteractive
+mkdir -p /data /iso /data/tpm
 
-RUN apt-get update && apt-get install -y \
-    qemu-system-x86 \
-    qemu-utils \
-    ovmf \
-    swtpm \
-    swtpm-tools \
-    novnc \
-    websockify \
-    wget \
-    curl \
-    && rm -rf /var/lib/apt/lists/*
+# FIX: Create OVMF_VARS.fd if missing
+if [ ! -f /data/OVMF_VARS.fd ]; then
+    cp /usr/share/OVMF/OVMF_VARS_4M.fd /data/OVMF_VARS.fd
+fi
 
-RUN mkdir -p /data /iso /tpm
+DISK="/data/windows11.qcow2"
 
-COPY start.sh /start.sh
-RUN chmod +x /start.sh
+# Create Windows disk if missing
+if [ ! -f "$DISK" ]; then
+    qemu-img create -f qcow2 "$DISK" 256G
+fi
 
-EXPOSE 6080 5900 3389
+# Create TPM state
+if [ ! -S /data/tpm/swtpm.sock ]; then
+    swtpm socket \
+        --tpm2 \
+        --tpmstate dir=/data/tpm \
+        --ctrl type=unixio,path=/data/tpm/swtpm.sock \
+        --daemon
+fi
 
-CMD ["/start.sh"]
+# Start noVNC
+websockify \
+    --web=/usr/share/novnc \
+    6080 \
+    localhost:5900 &
+
+# Start Windows 11
+exec qemu-system-x86_64 \
+    -enable-kvm \
+    -machine q35 \
+    -cpu host \
+    -m 32768 \
+    -smp 8 \
+    -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.fd \
+    -drive if=pflash,format=raw,file=/data/OVMF_VARS.fd \
+    -chardev socket,id=chrtpm,path=/data/tpm/swtpm.sock \
+    -tpmdev emulator,id=tpm0,chardev=chrtpm \
+    -device tpm-tis,tpmdev=tpm0 \
+    -drive file="$DISK",format=qcow2 \
+    -cdrom /iso/Win11.iso \
+    -netdev user,id=net0,hostfwd=tcp::3389-:3389 \
+    -device virtio-net-pci,netdev=net0 \
+    -vnc :0
